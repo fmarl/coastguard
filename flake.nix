08b4ce3da3d9dcb4d156c16d867ee3d66ca56024
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-06-18 22:21:28 +0200

    Project setup

---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Coastguard tries to enumerate phishing sites based on a website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+
+    code-nix = {
+      url = "github:fxttr/code-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        extensions.follows = "nix-vscode-extensions";
+      };
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+
+        fenix-channel = inputs.fenix.packages.${system}.latest;
+
+        fenix-toolchain = (fenix-channel.withComponents [
+          "rustc"
+          "cargo"
+          "clippy"
+          "rust-analysis"
+          "rust-src"
+          "rustfmt"
+          "llvm-tools-preview"
+        ]);
+
+        code = inputs.code-nix.packages.${system}.default;
+      in
+      {
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              rustfmt.enable = true;
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks;
+
+          nativeBuildInputs = with pkgs; [
+            fenix-toolchain
+            rust-analyzer
+            (code {
+              profiles = {
+                nix = {
+                  enable = true;
+                };
+                rust = {
+                  enable = true;
+                };
+              };
+            })
+          ];
+        };
+      });
+}
