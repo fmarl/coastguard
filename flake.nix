4138ce14e268a03c49313a88450c9d4c203709ea
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-07-26 07:40:29 +0200

    Switch to emacs with cider

--- flake.nix
+++ flake.nix
@@ -4,20 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    code-nix = {
-      url = "github:fmarl/code-nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        extensions.follows = "nix-vscode-extensions";
-      };
+    edinix = {
+      url = "github:fmarl/edinix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs = { self, nixpkgs, flake-utils, code-nix, ... }:
+  outputs = { self, nixpkgs, flake-utils, edinix, ... }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default = import ./shell.nix { inherit system nixpkgs code-nix; };
+      devShells.default = import ./shell.nix { inherit system nixpkgs edinix; };
     }
   );
 }
--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
-{ system, nixpkgs, code-nix }:
+{ system, nixpkgs, edinix }:
 
 let
   pkgs = import nixpkgs { inherit system; };
-  code = code-nix.packages.${system}.default {
+  emacs = edinix.packages.${system}.emacs {
     profiles.nix.enable = true;
     profiles.clojure.enable = true;
   };
 in pkgs.mkShell {
-  buildInputs = with pkgs; [ babashka code.editor code.tooling ];
+  buildInputs = with pkgs; [ babashka emacs.editor emacs.tooling ];
 }
